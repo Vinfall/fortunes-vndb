a0fb5eab2cedc33cf6235206103b0fe08752031d
Vinfall <91039000+Vinfall@users.noreply.github.com>
2025-04-17 12:36:06 +0800

    style: lint via ruff

--- format.py
+++ format.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 
-from urllib.request import urlopen
 import json
 import os
+from collections.abc import Generator
+from urllib.request import urlopen
 
 # Output files
 _OUTPUT_FOLDER = "output/"
@@ -18,10 +18,12 @@ os.makedirs(_OUTPUT_FOLDER, exist_ok=True)
 with open(_OUTPUT_FOLDER + _JSON_FILE, "w", encoding="utf-8") as f:
     json.dump(fortunes, f)
 
+
 # Convert to fortune file format
-def fortune_generator(fortunes):
+def fortune_generator(fortunes: list[dict]) -> Generator[str, None, None]:
     for fortune in fortunes:
         yield f"{fortune['quote']}\n\t-- {fortune['source']}"
+
 
 with open(_OUTPUT_FOLDER + _FORTUNE_FILE, "w") as file:
     # `%` is necessary for fortune file
@@ -29,4 +31,4 @@ with open(_OUTPUT_FOLDER + _FORTUNE_FILE, "w") as file:
         file.write(quote + "\n%\n")
 
 # Generate .dat file
-os.system(f"strfile -c % {_OUTPUT_FOLDER + _FORTUNE_FILE}")
+os.system(f"strfile -c % {_OUTPUT_FOLDER + _FORTUNE_FILE}")  # noqa: S605, intended shell injection
