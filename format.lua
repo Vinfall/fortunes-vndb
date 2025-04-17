1d7e8dff830d5a7e2f6d32ebc060caf394d98f72
Vinfall <91039000+Vinfall@users.noreply.github.com>
2025-04-17 12:44:14 +0800

    style: lint

--- format.lua
+++ format.lua
@@ -1,6 +1,6 @@
-local https = require("ssl.https")   -- luasec
-local cjson = require("cjson.safe")  -- cjson
-local lfs = require("lfs")           -- luafilesystem
+local https = require("ssl.https") -- luasec
+local cjson = require("cjson.safe") -- cjson
+local lfs = require("lfs") -- luafilesystem
 local os = require("os")
 
 -- Output files
@@ -11,38 +11,44 @@ local fortune_file = "vndb"
 -- Get query results
 local response_body, response_code = https.request("https://query.vndb.org/5c9a6037d875c238.json")
 if response_code ~= 200 then
-    error("Failed to get URL: " .. tostring(response_code))
+	error("Fail to get URL: " .. tostring(response_code))
 end
 
 local fortunes, err = cjson.decode(response_body)
 if err then
-    error("Failed to decode JSON: " .. tostring(err))
+	error("Fail to decode JSON: " .. tostring(err))
 end
 
 if not lfs.attributes(output_folder) then
-    lfs.mkdir(output_folder)
+	lfs.mkdir(output_folder)
 end
 
 local json_file_path = output_folder .. json_file
 local json_file_handle = io.open(json_file_path, "w")
+if not json_file_handle then
+	error("Fail to open file: " .. err)
+end
 json_file_handle:write(cjson.encode(fortunes))
 json_file_handle:close()
 
 -- Convert to fortune file format
 local function fortune_generator(fortunes)
-    local result = {}
-    for _, fortune in ipairs(fortunes) do
-        table.insert(result, fortune.quote .. "\n\t-- " .. fortune.source)
-    end
-    return result
+	local result = {}
+	for _, fortune in ipairs(fortunes) do
+		table.insert(result, fortune.quote .. "\n\t-- " .. fortune.source)
+	end
+	return result
 end
 
 -- Write fortunes to fortune file
 local fortune_file_path = output_folder .. fortune_file
 local file_handle = io.open(fortune_file_path, "w")
+if not file_handle then
+	error("Fail to open file: " .. err)
+end
 for _, quote in ipairs(fortune_generator(fortunes)) do
-    -- `%` is necessary for fortune file
-    file_handle:write(quote .. "\n%\n")
+	-- `%` is necessary for fortune file
+	file_handle:write(quote .. "\n%\n")
 end
 file_handle:close()
 
